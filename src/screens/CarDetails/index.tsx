e489c4565a8c130983154b6a6a1468fa2b0821c9
Evandro Assis <evandroassisdev@MacBook-Pro-de-Evandro.local>
2022-07-15 16:21:35 -0300

    button component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,4 +1,12 @@
+import AccelerationSvg from '@src/assets/acceleration.svg';
+import ExchangeSvg from '@src/assets/exchange.svg';
+import ForceSvg from '@src/assets/force.svg';
+import GasolineSvg from '@src/assets/gasoline.svg';
+import PeopleSvg from '@src/assets/people.svg';
+import SpeedSvg from '@src/assets/speed.svg';
+import { Accessory } from '@src/components/Accessory';
 import { BackButton } from '@src/components/BackButton';
+import { Button } from '@src/components/Button';
 import { ImageSlider } from '@src/components/ImageSlider';
 import React from 'react';
 
@@ -15,6 +23,8 @@ import {
   Period,
   Price,
   About,
+  Accessories,
+  Footer,
 } from './styles';
 
 export function CarDetails() {
@@ -45,12 +55,30 @@ export function CarDetails() {
           </Rent>
         </Details>
 
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sit
+          alias rerum corrupti cum et amet quod atque. Dolor molestiae doloribus
+          iure laboriosam quos neque debitis expedita, vel molestias fugiat?
+        </About>
         <About>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sit
           alias rerum corrupti cum et amet quod atque. Dolor molestiae doloribus
           iure laboriosam quos neque debitis expedita, vel molestias fugiat?
         </About>
       </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
